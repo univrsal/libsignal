e214b505b96897c7bc1895f9632b474f0074dcbf
univrsal <univrsal@protonmail.ch>
2020-03-06 21:55:06 +0100

    added object signal receiver

--- src/signal.hpp
+++ src/signal.hpp
@@ -30,7 +30,8 @@
 namespace signal {
 
     /**
-     * @brief The parameters class, contains a list of parameters used for calling signals
+     * \brief The parameters class, contains a list of parameters used for calling signals
+     * \class parameters
      */
     class parameters {
         std::map<std::string, std::pair<size_t, void *>> m_parameters;
@@ -137,6 +138,18 @@ namespace signal {
 	};
 
 	/**
+	 * \brief The receiver class is an interface class, which can be implemented
+	 * to allow sending of signals to objects
+	 * \class receiver
+	 * \defgroup signal++
+	 */
+	class receiver {
+	public:
+		virtual ~receiver() = 0;
+		virtual void receive(const parameters &param = parameters(), parameters *out = nullptr) = 0;
+	};
+
+	/**
 	 * \typedef Function pointer
 	 * \brief Structure for any method called by a signal
 	 * \param params	Parameters sent to this method by the signal caller
@@ -146,11 +159,14 @@ namespace signal {
 	typedef void (*signal_function)(const parameters &param, parameters *response);
 
 	/**
-	 * \brief The signal class, holds all receivers for this signal
+	 * \brief The signal class holds all receivers for this signal
+	 * they are separated in receiver methods and receiver objects
+	 * \class signal
 	 * \defgroup signal++
 	 */
 	class signal {
 		std::vector<signal_function> m_receivers;
+		std::vector<std::shared_ptr<receiver>> m_receiver_objects;
 	    public:
 		signal() = default;
 		~signal() = default;
@@ -166,6 +182,27 @@ namespace signal {
 		}
 
 		/**
+		 * \brief Adds r as the first receiver object
+		 * \param r the first receiver object for this signal
+		 * \defgroup signal++
+		 */
+		signal(receiver *r)
+		{
+			m_receiver_objects.emplace_back(std::shared_ptr<receiver>(r));
+		}
+
+		/**
+		 * \brief Adds r as the first receiver object using a shared pointer
+		 * to ensure that the object exists as long as this signal does
+		 * \param r the first receiver object for this signal
+		 * \defgroup signal++
+		 */
+		signal(std::shared_ptr<receiver>& r)
+		{
+			m_receiver_objects.emplace_back(r);
+		}
+
+		/**
 		 * \brief Invoke this signal
 		 * \param param the paramters to send to the receivers (optional)
 		 * \param response the response used by the receivers (shared by all receivers) (optional)
@@ -175,26 +212,60 @@ namespace signal {
 		{
 			for (const auto& recv : m_receivers)
 				recv(param, response);
+			for (const auto& recv_obj : m_receiver_objects)
+				recv_obj->receive(param, response);
 		}
 
 		/**
 		 * \brief Add a receiver for this signal
 		 * \param f the receiver function
-		 * \return true if the signal could be added, false if it already is registered
+		 * \return true if the function could be added, false if it is already registered
 		 * \defgroup signal++
 		 */
 		bool add_receiver(signal_function f)
 		{
+			if (!f)
+				return false;
 			if (std::find(m_receivers.begin(), m_receivers.end(), f) == m_receivers.end()) {
 				m_receivers.emplace_back(f);
 				return true;
 			}
 			return false;
 		}
+
+		/**
+		 * \brief Add a receiver object for this signal using a shared pointer
+		 * to ensure that the object exists for as long as this signal does
+		 * \param r the receiver object
+		 * \return true if the receiver object could be added, false if it is already registered
+		 * \defgroup signal++
+		 */
+		bool add_receiver_obj(std::shared_ptr<receiver> &&r)
+		{
+			if (!r)
+				return false;
+			if (std::find(m_receiver_objects.begin(), m_receiver_objects.end(), r) == m_receiver_objects.end()) {
+				m_receiver_objects.emplace_back(r);
+				return true;
+			}
+			return false;
+		}
+
+		/**
+		 * \brief Add a receiver object for this signal
+		 * \param r the receiver object
+		 * \return true if the receiver object could be added, false if it is already registered
+		 * \defgroup signal++
+		 */
+		bool add_receiver_obj(receiver *r)
+		{
+			return add_receiver_obj(std::shared_ptr<receiver>(r));
+		}
 	};
 
 	/**
 	 * \brief The manager class, manages signals
+	 * \class manager
 	 * \defgroup signal++
 	 */
 	class manager {
@@ -222,7 +293,7 @@ namespace signal {
 		/**
 		 * \brief Add a signal to the manager
 		 * \param id the id of the signal to register
-		 * \param fun the signal function for this signal (optional)
+		 * \param fun the first receiver function for this signal (optional)
 		 * \return true if the signal could be added, false if the id already exists
 		 */
 		bool add(const std::string &id, signal_function fun = nullptr)
@@ -232,7 +303,27 @@ namespace signal {
 				m_signals[id] = signal(fun);
 				return true;
 			}
-			return sig->second.add_receiver(fun);
+			if (fun)
+				return sig->second.add_receiver(fun);
+			return false;
+		}
+
+		/**
+		 * \brief Add a signal to the manager
+		 * \param id the id of the signal to register
+		 * \param r the first receiver object for this signal (optional)
+		 * \return true if the signal could be added, false if the id already exists
+		 */
+		bool add(const std::string &id, receiver *r = nullptr)
+		{
+			auto sig = m_signals.find(id);
+			if (sig == m_signals.end()) {
+				m_signals[id] = signal(r);
+				return true;
+			}
+			if (r)
+				return sig->second.add_receiver_obj(r);
+			return false;
 		}
 	};
 };
